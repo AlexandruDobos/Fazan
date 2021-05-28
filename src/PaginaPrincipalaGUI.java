1a3926d7774ae965dcc66719918f0c39f54e819b
Alexandru Dobos <dobosalex77@yahoo.com>
2021-05-28 14:52:47 +0300

    Initial commit

---
+++ src/PaginaPrincipalaGUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PaginaPrincipalaGUI extends JFrame{
+    private JPanel mainPanel;
+    private JTextField numeUtilizator;
+    private JButton start;
+    private JLabel name;
+    private JLabel bunVenit;
+    private JLabel copyRight;
+
+    public PaginaPrincipalaGUI(String appName){
+        super(appName);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.mainPanel.setPreferredSize(new Dimension(500,500));
+        this.pack();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new PaginaPrincipalaGUI("Fazan");
+        frame.setVisible(true);
+    }
+}
